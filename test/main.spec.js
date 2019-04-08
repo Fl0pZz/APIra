9671f4087aa94d02e057713b7bb365c807ab5432
fahreevr <fahreevrafael@yandex.ru>
2019-04-08 12:54:17 +0300

    Feat: add some tests

--- test/main.spec.js
+++ test/main.spec.js
@@ -9,11 +9,7 @@ beforeEach(() => {
   api = new APIra({ adapter: mockAxios })
 })
 
-it ('Create APIra', () => {
-  expect(api instanceof APIra).toBeTruthy()
-})
-
-it ('Test exception', async () => {
+it ('Test exception: Undefined url', async () => {
   expect.assertions(2)
 
   expect(() => api.params({ id: 10 })).toThrowError(/Undefined url/)
@@ -25,9 +21,7 @@ it ('Test exception', async () => {
   }
 })
 
-it ('Test requests', () => {
-  const catchFn = jest.fn()
-  const thenFn = jest.fn()
+it ('Test generated axios config', () => {
   const clientData = { a: false, b: 'b'}
 
   api
@@ -45,10 +39,39 @@ it ('Test requests', () => {
   );
 })
 
-it ('Test config', () => {
+it ('Test hooks', () => {
+  const hook = jest.fn(async (ctx, next) => {
+    await next()
+  })
   api
+    .hook(hook)
     .url('http://testserver.ru/api/v1/user/:id')
     .params({ id: 10 })
     .data({ a: false, b: 'b'})
     .GET()
+})
+
+it ('Test extend', () => {
+  const hook = async (ctx, next) => {
+    await next()
+  }
+  api
+    .hook(hook)
+    .url('http://testserver.ru/api/v1/user/:id')
+    .params({ id: 10 })
+    .options({
+      headers: {
+        Authorization: 'Bearer SomeToken'
+      }
+    })
+  const extend = api.extend()
+  expect(extend._config).toEqual(api._config)
+
+  extend.options({
+    headers: {
+      Authorization: 'Bearer OtherToken'
+    }
+  })
+
+  expect(extend._config).not.toEqual(api._config);
 })
